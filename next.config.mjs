d2c61a2c59a58096fbf97bc2e380e81e281aaa43
Axel Yael Zambrano Flores <lukanewf77@gmail.com>
2025-09-06 13:57:18 -0600

    Ahora con PWA

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
 
-export default nextConfig;
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development'
+});
+
+const nextConfig = {
+  // Aquí puedes añadir otras configuraciones de Next.js si las necesitas en el futuro
+};
+
+module.exports = withPWA(nextConfig);
